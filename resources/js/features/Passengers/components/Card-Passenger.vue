00f85947ba5787c7511587eec4df6bd5472aa549
StReloK93 <strelok0493@gmail.com>
2024-08-05 22:58:13 +0500

    fast

--- resources/js/features/Passengers/components/Card-Passenger.vue
+++ resources/js/features/Passengers/components/Card-Passenger.vue
@@ -1,57 +1,63 @@
 <template>
-   <v-card class="pa-2 pb-0 border-primary border-opacity-100 bg-white elevation-1 rounded-e passenger-card">
-      <main class="d-flex align-end mb-3">
-         <aside class="pr-2 leading-none">
-            <v-label class="text-caption mr-1">
-               Qayerdan
-            </v-label>
-            <div>
-               {{ props.passenger?.start.name }}
+   <v-card class=" border-primary border-opacity-100 bg-white elevation-1 rounded-e passenger-card">
+      <section @click="$emit('activate', props.passenger?.id)" class="pa-2 pb-0" v-ripple>
+         <main class="d-flex align-end mb-3">
+            <aside class="pr-2 leading-none">
+               <v-label class="text-caption mr-1">
+                  Qayerdan
+               </v-label>
+               <div>
+                  {{ props.passenger?.start.name }}
+               </div>
+            </aside>
+            <div class="middle-line mb-2 position-relative">
+               <v-icon color="primary" class="position-absolute chevron-left-icon">mdi-chevron-right</v-icon>
             </div>
-         </aside>
-         <div class="middle-line mb-2 position-relative">
-            <v-icon color="primary" class="position-absolute chevron-left-icon">mdi-chevron-right</v-icon>
-         </div>
-         <aside class="pl-2 leading-none text-right">
-            <v-label class="text-caption mr-1">
-               Qayerga
-            </v-label>
-            <div>
-               {{ props.passenger?.end.name }}
-            </div>
-         </aside>
-      </main>
-      <main class="mb-2">
-         <span v-for="item in props.passenger?.count">
-            <v-icon size="small" color="primary">mdi-account</v-icon>
-         </span>
-      </main>
-      <section class="d-flex justify-space-between align-center mx-n2">
-         <div v-if="props.passenger?.ride_time" class="leading-none pl-2">
-            <p class="text-grey-darken-1 mb-n2 text-caption">
-               {{ moment(passenger.ride_time).format('HH:mm') }}
-            </p>
-            <span class="text-uppercase text-h6 font-weight-bold text-grey-darken-3">
-               {{ moment(passenger.ride_time).format('D-MMMM') }}
+            <aside class="pl-2 leading-none text-right">
+               <v-label class="text-caption mr-1">
+                  Qayerga
+               </v-label>
+               <div>
+                  {{ props.passenger?.end.name }}
+               </div>
+            </aside>
+         </main>
+         <main class="mb-2">
+            <span v-for="item in props.passenger?.count">
+               <v-icon size="small" color="primary">mdi-account</v-icon>
             </span>
-         </div>
-         <div v-else style="height: 44px;"></div>
-         <v-chip variant="tonal" color="primary" class="font-weight-medium rounded-e-0">
-            {{ format(props.passenger?.price, moneyConfig) }} so'm
-         </v-chip>
+         </main>
+         <section class="d-flex justify-space-between align-center mx-n2">
+            <div class="leading-none pl-2">
+               <p class="text-grey-darken-1 mb-n2 text-caption">
+                  {{ moment(props.passenger?.ride_time).format('HH:mm') }}
+               </p>
+               <span class="text-uppercase text-h6 font-weight-bold text-grey-darken-3">
+                  {{ moment(props.passenger?.ride_time).format('D-MMMM') }}
+               </span>
+            </div>
+            <v-chip variant="tonal" color="primary" class="font-weight-medium rounded-e-0">
+               {{ format(props.passenger?.price, moneyConfig) }} so'm
+            </v-chip>
+         </section>
       </section>
-      <section class="bg-grey-lighten-4 elevation-1 mx-n2 d-flex">
-         <RouterLink :to="{ name: 'passenger-offers', params: { id: props.passenger?.id } }"  v-if="(Auth.isAnyAdmins || isMyAdd(props.passenger))">
-            <v-btn size="x-small" variant="text" icon="mdi-message-badge"/>
-         </RouterLink>
-         <a :href="`tel:+998${props.passenger?.phone}`" v-if="Auth.isAnyAdmins">
-            <v-btn size="x-small" variant="text" icon="mdi-phone"  color="teal"/>
-         </a>
-         <div>
-            <EditForm :id="props.passenger?.id"></EditForm>
-            <v-btn size="x-small" variant="text" icon="mdi-delete" @click="passengerDelete"/>
+      <v-expand-transition>
+         <div v-if="props.activeCard == props.passenger?.id" class="px-2">
+            <section class="bg-grey-lighten-4 elevation-1 mx-n2 d-flex">
+               <RouterLink :to="{ name: 'passenger-offers', params: { id: props.passenger?.id } }"
+                  v-if="(Auth.isAnyAdmins || isMyAdd(props.passenger))">
+                  <v-btn size="x-small" variant="text" icon="mdi-message-badge" />
+               </RouterLink>
+               <a :href="`tel:+998${props.passenger?.phone}`" v-if="Auth.isAnyAdmins">
+                  <v-btn size="x-small" variant="text" icon="mdi-phone" color="teal" />
+               </a>
+               <div>
+                  <EditForm :id="props.passenger?.id"></EditForm>
+                  <v-btn size="x-small" variant="text" icon="mdi-delete" @click="passengerDelete" />
+               </div>
+            </section>
          </div>
-      </section>
+      </v-expand-transition>
    </v-card>
 </template>
 
@@ -62,12 +68,13 @@ import { moneyConfig } from '@/modules/constants'
 import moment from '@/modules/moment'
 import { useAuthStore } from '@/store/useAuthStore'
 import { useMainStore } from '@/store/useMainStore'
-const props = defineProps(['passenger', 'crud'])
+import { ref } from 'vue';
+const props = defineProps(['passenger', 'crud', 'activeCard'])
 
 const Auth = useAuthStore()
 const store = useMainStore()
 const passengerStore = usePassengerStore()
-
+const expand = ref(false)
 function passengerDelete() {
    store.dialog.open(() => {
       store.dialog.title = "Qatnovni o'chirmoqchimisiz ?"
@@ -77,7 +84,7 @@ function passengerDelete() {
 }
 
 
-function isMyAdd(passenger){
+function isMyAdd(passenger) {
    return Auth.user?.id == passenger?.user_id
 }
 </script>
--- resources/js/pages/general/PassengersPage.vue
+++ resources/js/pages/general/PassengersPage.vue
@@ -3,7 +3,7 @@
 		<v-spacer class="position-relative">
 			<main class="position-absolute top-0 left-0 right-0 bottom-0 overflow-y-auto overflow-x-hidden px-1">
 				<TransitionGroup name="list">
-					<Card :crud="true" v-for="passenger in filtered_passengers" :passenger="passenger" :key="passenger.id" class="mb-2"/>
+					<Card @activate="selectActive" :activeCard="activeCard" :crud="true" v-for="passenger in filtered_passengers" :passenger="passenger" :key="passenger.id" class="mb-2"/>
 				</TransitionGroup>
 			</main>
 		</v-spacer>
@@ -16,11 +16,16 @@ import { onMounted, onUnmounted, computed } from 'vue'
 import { Card, AddForm, PassengerRepository, usePassengerStore } from '@/features/Passengers';
 import { useAuthStore } from '@/store/useAuthStore'
 import { useMainStore } from '@/store/useMainStore'
-
+import { ref } from 'vue';
 const AuthStore = useAuthStore()
 const AppStore = useMainStore()
 const passengerStore = usePassengerStore()
+const activeCard = ref(null)
 
+function selectActive(id){
+	if(activeCard.value == id) return activeCard.value = null 
+	else return activeCard.value = id
+}
 const filtered_passengers = computed(() => {
 	if (AuthStore.user.role_id == 4)
 		return passengerStore.passengers?.filter((passenger) => passenger.start_city == AppStore.city.id)
